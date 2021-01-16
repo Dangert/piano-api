67d906d4b4e89dea154f1ca9a4d49bc7c87f3c5c
Dangert <30866415+Dangert@users.noreply.github.com>
2021-01-16 17:32:03 +0200

    remove comment from server.js

--- server.js
+++ server.js
@@ -56,19 +56,3 @@ app.get('/users/exist/:username', users.userExist(db));
 app.listen(process.env.PORT || 3000, () => {
   console.log("app is running on port " + process.env.PORT);
 })
-
-
-/*
-CREATE TABLE users (id serial PRIMARY KEY, username text UNIQUE NOT NULL, joined TIMESTAMP NOT NULL, total_score INT DEFAULT 0, total_games INT DEFAULT 0);
-INSERT INTO users(id, username, joined) VALUES ('125', 'dan', '2016-06-22 19:10:25-07');
-
-CREATE TABLE login (id serial PRIMARY KEY, hash varchar(100) NOT NULL, username text UNIQUE NOT NULL);
-
-CREATE TABLE games (id serial PRIMARY KEY, user_id serial NOT NULL, level text NOT NULL, score INT NOT NULL, created_at TIMESTAMP NOT NULL);
-INSERT INTO games(id, user_id, level, score, created_at) VALUES (1, 'EASY', 3, '2016-06-22 19:10:25-07')
-
-SELECT COUNT(Games.id) as total_games, SUM(Games.score) as total_score from (SELECT * FROM games WHERE user_id='11') AS Games;
-
-
-
-*/
